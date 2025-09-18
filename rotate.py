968148daa9ea7ca8e4f5d1b23c886c058af378a5
Zeher <siamsaharear010@gmail.com>
2025-09-18 22:32:29 +0600

    fix image getting cut off when rotated

--- rotate.py
+++ rotate.py
@@ -7,7 +7,7 @@ def rotate_image(image, angle, center=True, clockwise=True, scale=1):
     if center == True:
         center = (width//2, height//2)
     else:
-        center_x, center_y = int(input("enter center form x axis and y axis: "))
+        center_x, center_y = map(int, input("enter center value of x and y: ").split(" "))
         center = (center_y, center_x)
     if clockwise != True:
         angle = angle
@@ -15,7 +15,13 @@ def rotate_image(image, angle, center=True, clockwise=True, scale=1):
         angle = -angle
     
     M = cv2.getRotationMatrix2D(center=center, angle=angle, scale=scale)
-    rotated_image = cv2.warpAffine(image, M=M, dsize=(width, height))
+    cos = abs(M[0, 0])
+    sin = abs(M[0, 1])
+    new_width = int((height * sin) + (width * cos))
+    new_height = int((height * cos) + (width * sin))
 
+    M[0, 2] += (new_width / 2) - center[0]
+    M[1, 2] += (new_height / 2) - center[1]
+    rotated_image = cv2.warpAffine(image, M=M, dsize=(new_width, new_height))
     return rotated_image
 
