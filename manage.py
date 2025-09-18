e3dc64363e42e4ea742b5d062e3c45d5c0b2a439
Zeher <siamsaharear010@gmail.com>
2025-09-18 16:12:36 +0600

    write a function for available images

---
+++ manage.py
@@ -0,0 +1,23 @@
+import os
+import cv2
+
+def image_options(current_directory=True, image_folder=None):
+    current_directory_path = os.getcwd()
+    all_images = []
+    
+    if current_directory == True:
+        image_folder_path = current_directory_path
+    else: 
+        image_folder_path = os.path.join(current_directory, image_folder)
+    
+    if len(os.listdir(image_folder_path)) != 0:
+        for file in os.listdir(image_folder_path):
+            if file.split(".")[-1] in ("png", "jpg", "tiff"):
+                current_file_path = os.path.join(image_folder_path, file)
+                all_images.append(current_file_path)
+            else:
+                print("file not image")
+    else:
+        print("directory empty")
+
+
